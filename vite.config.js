7e35a7600c45d922a52cc4410a262df303e6e35c
hato1198 <hato1198@gmail.com>
2025-10-19 23:56:39 +0900

    Implemented Vite and added the English page

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import htmlMinifier from 'vite-plugin-html-minifier-terser';
+
+export default defineConfig({
+  root: 'src',
+  base: '/',
+  plugins: [
+    htmlMinifier({
+      removeComments: true,
+    })
+  ],
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    sourcemap: false,
+    minify: 'esbuild',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        'ja/index': resolve(__dirname, 'src/ja/index.html'),
+      }
+    }
+  },
+});
